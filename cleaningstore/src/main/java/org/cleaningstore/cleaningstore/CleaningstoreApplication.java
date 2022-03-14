0e6937a8ddf622fbea7a56b3de58639ae65c2b4d
kristya <shiriagina@yahoo.com>
2022-03-14 15:13:45 +0100

    created spring boot project

---
+++ cleaningstore/src/main/java/org/cleaningstore/cleaningstore/CleaningstoreApplication.java
@@ -0,0 +1,13 @@
+package org.cleaningstore.cleaningstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleaningstoreApplication.class, args);
+	}
+
+}
---
+++ cleaningstore/src/test/java/org/cleaningstore/cleaningstore/CleaningstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cleaningstore.cleaningstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
