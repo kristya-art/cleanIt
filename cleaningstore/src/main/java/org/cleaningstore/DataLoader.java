eba71330876e89479d480ac67e545990662ce74f
kristya <shiriagina@yahoo.com>
2022-03-15 17:04:39 +0100

    added customers to the db

--- cleaningstore/src/main/java/org/cleaningstore/DataLoader.java
+++ cleaningstore/src/main/java/org/cleaningstore/DataLoader.java
@@ -36,21 +36,33 @@ public class DataLoader implements ApplicationRunner {
 //        OrderItem orderItem = new OrderItem(book,1);
 //        OrderItem orderItem2 = new OrderItem(book2,3);
 
-        Customer customer1 = new Customer("Alic", "Bon","alic@gmx.com","alicb^^^^",new Address("Mainstreet 45","Zuchwil","SO","4528","Schweiz"),new CreditCard(CreditCardType.VISA,"1234",06,2023));
+        Customer customer1 = new Customer("Alic", "Bon","alic@gmx.com","alicb^^^^",new Address("Mainstreet 45","Zuchwil","SO","4528","Schweiz"),new CreditCard(CreditCardType.VISA,"1234 5678 9123 4567" ,06,2023));
+        Customer customer2 = new Customer("Benni", "Bellini","benni@gmx.com","bebe^^^^",new Address("Cherrystreet 5","Bern","BE","3019","Schweiz"),new CreditCard(CreditCardType.VISA,"1234 8976 8934 3487",03,2023));
+        Customer customer3 = new Customer("Zamira", "Karimi","zaka@gmx.com","zaka^^^^",new Address("Mangostreet 1","Biel","BE","2500","Schweiz"),new CreditCard(CreditCardType.MASTERCARD,"9836 3635 5324 2837",05,2025));
+        Customer customer4 = new Customer("Alessandro", "Pinelli","shiriagina@yahoo.com","alpi^^^",new Address("Pineapplestreet 13","Zurich","ZU","8000","Schweiz"),new CreditCard(CreditCardType.VISA,"2963 7437 3737 9538",01,2024));
+
+
+
 //        Customer Customer3 = new Customer("Make", "make@gmx.com");
 //        Customer Customer2 = new Customer("Karol", "karol@gmx.com");
 
-//			ArrayList<Customer> list = new ArrayList<>();
-//			list.add(Customer1);
-//			list.add(Customer2);
-//			list.add(Customer3);
-//        List<OrderItem> orderItems = new ArrayList<>();
+			ArrayList<Customer> list = new ArrayList<>();
+			list.add(customer1);
+			list.add(customer2);
+			list.add(customer3);
+            list.add(customer4);
+
+            for(Customer c:list){
+                customerRepository.save(c);
+            }
+
+ //       List<OrderItem> orderItems = new ArrayList<>();
 //        orderItems.add(orderItem);
 //        orderItems.add(orderItem2);
 //        Order newOrder = orderService.createOrder(customer1,orderItems);orderItems
 
 //        Order order = new Order(orderItems,OrderStatus.ACCEPTED,new BigDecimal(89.0),new Date(),customer1);
-       customerRepository.save(customer1);
+
 //        orderRepository.save(order);
     }
 }
